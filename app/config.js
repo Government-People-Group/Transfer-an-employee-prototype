a0e2dba804b5a36e51f6115020b116ebe2bc19a8
BeccyHazell <rebecca.hazell@cabinetoffice.gov.uk>
2022-01-14 21:29:03 +0000

    Started to add pages to the user journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Transfer an employee across the Civil Service',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,103 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/', function (req, res) {
+  req.session.destroy()
+  res.render('index', {
+  })
+})
+
+// Applicant-details
+
+router.post('/service/personal-details', function (req, res) {
+  var errors = []
+  var nameHasError = false
+  var emailHasError = false
+  var dobHasError = false
+
+  if (req.session.data['full-name'] === '') {
+    nameHasError = true
+    errors.push({
+      text: "Enter your full name",
+      href: '#full-name'
+    })
+  }
+
+  if (req.session.data['dob-year'] === '') {
+    dobHasError = true
+    errors.push({
+      text: 'Date of birth must include a year',
+      href: '#year'
+    })
+  }
+
+  if (req.session.data['email'] === '') {
+    emailHasError = true
+    errors.push({
+      text: "Enter your email address",
+      href: '#email'
+    })
+  }
+
+
+  if (nameHasError || dobHasError || emailHasError) {
+    res.render('service/personal-details', {
+      errorName: nameHasError,
+      errorDob: dobHasError,
+      errorEmail: emailHasError,
+      errorList: errors
+    })
+  } else {
+    res.redirect('home-address')
+  }
+})
+
+// home-address-details
+
+router.post('/service/home-address', function (req, res) {
+  var errors = []
+  var postcodeHasError = false
+
+  if (req.session.data['postcode'] === '') {
+    postcodeHasError = true
+    errors.push({
+      text: 'Enter a postcode',
+      href: '#postcode'
+    })
+  }
+
+  if (postcodeHasError) {
+    res.render('service/home-address', {
+      errorPostcode: postcodeHasError,
+      errorList: errors
+    })
+  } else {
+    res.redirect('/service/confirm-home-address')
+  }
+})
+
+// contact-details
+
+router.post('/service/home-address', function (req, res) {
+  var errors = []
+  var EmailHasError = false
+
+  if (req.session.data['postcode'] === '') {
+    EmailHasError = true
+    errors.push({
+      text: 'Enter a postcode',
+      href: '#postcode'
+    })
+  }
+
+  if (postcodeHasError) {
+    res.render('service/home-address', {
+      errorPostcode: postcodeHasError,
+      errorList: errors
+    })
+  } else {
+    res.redirect('/service/confirm-home-address')
+  }
+})
+
 module.exports = router
